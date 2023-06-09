19dc4553a60c72d30a4ec54422eb0df837cde0db
Siddharth Shanker <siddharth.shanker.24.1@mountblue.tech>
2023-06-09 18:14:25 +0530

    Final Commit

--- Siddharth-http-server.py
+++ Siddharth-http-server.py
@@ -44,15 +44,12 @@ class Server(http.server.BaseHTTPRequestHandler):
                 self.wfile.write(f"Response with status code: {statusCode}".encode())
          
         elif len(path_parts) >=3 and path_parts[1] == 'delay':
-            try:
                 delay_seconds = int(path_parts[2])
                 time.sleep(delay_seconds)  
                 self.send_response(200)
                 self.send_header('Content-type', "text/json")
                 self.end_headers()
-                self.wfile.write(f"Time delayed was {delay_seconds} seconds".encode())     
-            except:
-                self.send_res  
+                self.wfile.write(f"Time delayed was {delay_seconds} seconds".encode())                
         else:
             self.send_response(404);
             self.end_headers();

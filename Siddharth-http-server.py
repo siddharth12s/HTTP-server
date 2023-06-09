be2b2cd216fae6b3ab0e0766036370d86e43c59b
Siddharth Shanker <siddharth.shanker.24.1@mountblue.tech>
2023-06-09 17:58:32 +0530

    Completed HTTP drill commit

---
+++ Siddharth-http-server.py
@@ -0,0 +1,64 @@
+import http.server
+import time
+
+class Server(http.server.BaseHTTPRequestHandler):
+
+    
+    def do_GET(self):
+        path_parts = self.path.split('/')
+        if self.path == '/html':
+            self.send_response(200)
+            self.send_header('Content-type', 'text/html')
+            self.end_headers()
+            html_content = ""
+            with open('first.html', 'r') as file:
+                html_content = file.read()
+            
+            self.wfile.write(html_content.encode())
+            
+            
+        elif self.path == '/json':
+            self.send_response(200)
+            self.send_header('Content-type', 'text/json')
+            self.end_headers()
+            with open('first.json', 'rb') as page:
+                self.wfile.write(page.read())  
+        
+        elif self.path == '/uuid':
+            self.send_response(200)
+            self.send_header('Content-type', 'text/json')
+            self.end_headers()
+            with open('uuid.json', 'rb') as page:
+                self.wfile.write(page.read())  
+        
+        elif len(path_parts) >=3 and path_parts[1]=='status':
+                statusCode = int(path_parts[2])
+                self.send_response(statusCode)
+                self.send_header('Content-type', 'text/html')
+                self.end_headers()
+                self.wfile.write(f"Response with status code: {statusCode}".encode())
+         
+        elif len(path_parts) >=3 and path_parts[1] == 'delay':
+                delay_seconds = int(path_parts[2])
+                time.sleep(delay_seconds)  
+                self.send_response(200)
+                self.send_header('Content-type', "text/json")
+                self.end_headers()
+                self.wfile.write(f"Time delayed was {delay_seconds} seconds".encode())       
+        else:
+            self.send_response(404);
+            self.end_headers();
+    
+
+
+PORT =9000
+
+my_server = http.server.HTTPServer(('localhost', PORT), Server)
+try:
+    # Serve indefinitely until interrupted
+    print("Server running on http://localhost/9000")
+    my_server.serve_forever()
+except KeyboardInterrupt:
+    # Close the server when interrupted by Ctrl+C
+    my_server.server_close()
+    print("Server closed")
